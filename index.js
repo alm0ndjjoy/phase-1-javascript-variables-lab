2cd883f5338b999e2b702f33cc785b53e941df4c
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-11-01 11:41:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Code your solution in this file!
 const companyName = "Scuber";
-
-let mostProfitableNeighborhood = "Chelsea";
-
-
+let mostProfitableNeighborhood = "Chelsea"
 let companyCeo = "Susan Smith"
